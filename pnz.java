7f6a6d6b82ad3ecccbcddc94bd729dfa9e976e1f
Jeevitha-m <jeeveemega@gmail.com>
2017-06-03 15:38:37 +0530

    Update pnz.java

--- pnz.java
+++ pnz.java
@@ -7,7 +7,8 @@ public static void main(String args[])
 Scanner s=new Scanner(System.in);
 int a=s.nextInt();
 if(a>0)
-{System.out.println("positive");
+{
+  System.out.println("positive");
 }
 else if(a<0)
 {
