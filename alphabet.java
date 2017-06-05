fe6fb21f58cd70bc3fc52e1021dad60b888b468c
Jeevitha-m <jeeveemega@gmail.com>
2017-06-05 18:42:49 +0530

    Update alphabet.java

--- alphabet.java
+++ alphabet.java
@@ -5,6 +5,6 @@ class alphabet
 public static void main(String args[])
 {
 Scanner s=new Scanner(System.in);
-String ch=s.nextLine();
-System.out.println(isLetter(ch));
+String ch=s.next().charAt(0);
+System.out.println(Character.isLetter(ch));
 }}
