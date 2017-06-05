bf39791a4f97fe4235f010d596f65e8d528f5da8
Jeevitha-m <jeeveemega@gmail.com>
2017-06-05 18:23:24 +0530

    Create alphabet.java

---
+++ alphabet.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.util.*;
+class alphabet
+{
+public static void main(String args[])
+{
+Scanner s=new Scanner(System.in);
+String ch=s.nextLine();
+System.out.println(isLetter(ch));
+}}
