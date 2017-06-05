125152313d74021ef855d79d24eef782c2506f49
Jeevitha-m <jeeveemega@gmail.com>
2017-06-05 21:24:27 +0530

    Update target.java

--- target.java
+++ target.java
@@ -2,15 +2,16 @@ import java.io.*;
 import java.util.*;
 class target
 {
-public static void main(String args[]);
-{int i,j;
+public static void main(String args[])
+{int i,j,sum=0;
 Scanner s=new Scanner(System.in);
 int ch[]=new int[args.length];
-int t=s.nextInt();
+
 for(i=0;i<args.length;i++)
 {
 ch[i]=Integer.parseInt(args[i]);
 }
+ int t=s.nextInt();
 for(j=0;j<ch.length;j++)
 {
 for(i=j+1;i<ch.length;i++)
@@ -22,4 +23,7 @@ System.out.println(ch[j]+" "+ch[i]);
 }else
 {
 sum=0;
-}}}}}
+}}}
+if(sum==0)
+System.out.println("no valuecmatches");
+}}
